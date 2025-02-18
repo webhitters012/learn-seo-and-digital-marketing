d5a8d5f6679f531b7eae49294f42fee94cf50e5a
webhitters0123 <xyz.webhitters@gmail.com>
2025-02-18 11:44:57 +0530

    hh

--- src/components/Founder/Founder.jsx
+++ src/components/Founder/Founder.jsx
@@ -3,12 +3,14 @@ import Header from "../header";
 import Footer from "../Footer";
 import Navbar from "../Navbar";
 import ProfileSection from "./Profile";
+import FounderSection from "./FounderSection"
 
 const Founder = () => {
     return(
         <div>
             <Header/>
             <Navbar/>
+            <FounderSection/>
             <ProfileSection/>
             <Footer/>
         </div>
---
+++ src/components/Founder/FounderSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./FounderSection.css"; // Import the CSS file
+
+export default function FounderSection() {
+  return (
+    <div className="founder-section">
+      <div className="overlay"></div>
+      <h1 className="founder-text">Our Founder</h1>
+    </div>
+  );
+}
